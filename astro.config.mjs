6b5eee2e5d3c29cf17ac664031210522ba0e0d24
always-open-io <aj@openalways.io>
2026-05-15 22:28:22 -0700

    Add /privacy-policy redirect to /privacy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://openalways.io',
-    output: 'static',
+      site: 'https://openalways.io',
+      output: 'static',
+      redirects: {
+              '/privacy-policy': '/privacy',
+      },
 });
